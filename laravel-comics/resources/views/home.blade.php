9c0f558727b36281f374a76e4d332fa439a46e16
Michele <micheleierniero@gmail.com>
2021-10-27 15:15:48 +0200

    struttura

---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('jumbotron')
+    <h1>
+        JUMBOTRON
+    </h1>
+@endsection
+
+@section('series')
+    <h1>
+        SERIES
+    </h1>
+@endsection
+
+@section('shop')
+    <h1>
+        SHOP
+    </h1>
+@endsection
